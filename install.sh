c71e874fc962e3aab5fee309c5b28710bb53ff32
Yuki Takei <yuki@weseek.co.jp>
2017-04-04 02:28:31 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-tempfile=/tmp/dotfiles.tar.gz
-
 # clone
 git clone https://github.com/yuki-takei/dotfiles.git
 
 # Create symlink
 ln -s dotfiles/.tmux.conf
-
-# Cleanup
-rm -rf ${tempfile}

6b0cf7e2ce7383c2be30301c9c526b3ec5e5efca
Yuki Takei <yuki@weseek.co.jp>
2017-04-04 02:25:05 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-tempfile=/tmp/dotfiles.zip
-workspace=~/.dotfiles
+tempfile=/tmp/dotfiles.tar.gz
 
 # Download zipped installer
-curl -LSfs -o ${tempfile} https://github.com/yuki-takei/dotfiles/archive/master.zip
+curl -LSfs -o ${tempfile} https://github.com/yuki-takei/dotfiles/archive/master.tar.gz
 
 # Unzip installer into workspace
-unzip -oq ${tempfile} -d ${workspace}
+mkdir -p dotfiles
+tar zxf ${tempfile} -C dotfiles --strip-components 1
+
+# Create symlink
+ln -s dotfiles/.tmux.conf
 
 # Cleanup
-rm -rf ${tempfile} ${workspace}
+rm -rf ${tempfile}
